cbb483f4701eea626a904bee603238e2a729f969
ketianBai <85915101+ketianBai@users.noreply.github.com>
2021-09-24 20:11:03 -0700

    Update PoseModule.py
    change findAngle() function 
    angle=np.abs(angle)
            
            if angle>180.0:
                angle=360-angle

--- scripts/PoseModule.py
+++ scripts/PoseModule.py
@@ -58,8 +58,11 @@ class poseDetector():
         # Calculate the Angle
         angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
                              math.atan2(y1 - y2, x1 - x2))
-        if angle < 0:
-            angle += 360
+        angle=np.abs(angle)
+        
+        if angle>180.0:
+            angle=360-angle
+
 
         # print(angle)
 
