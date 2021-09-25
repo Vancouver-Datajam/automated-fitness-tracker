f6011ce1a3e5bf168c035c716f0ef8be4f386b4c
ketianBai <85915101+ketianBai@users.noreply.github.com>
2021-09-24 20:31:01 -0700

    Update PoseModule.py

--- scripts/PoseModule.py
+++ scripts/PoseModule.py
@@ -8,6 +8,7 @@ import cv2
 import mediapipe as mp
 import time
 import math
+import numpy as np
 
 
 class poseDetector():
@@ -58,6 +59,7 @@ class poseDetector():
         # Calculate the Angle
         angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
                              math.atan2(y1 - y2, x1 - x2))
+        
         angle=np.abs(angle)
         
         if angle>180.0:
